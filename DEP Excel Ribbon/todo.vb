50e53dff292a1fd49546c87ff2de6739772c5f2c
Martin Klefas-Stennett <cleverbum@hotmail.com>
2018-12-28 15:56:45 +0000

    Todo Changes typos in pivotmail

--- DEP Excel Ribbon/todo.vb
+++ DEP Excel Ribbon/todo.vb
@@ -1,4 +1,6 @@
 ﻿Module todo
     '   This is a list of outstanding tasks, and contains no functional code. Everything should remain commented out - even if it loooks like code
-    ' 
+
+    '  check pivotmail with Duncan/Jenny
+    '  change pivotmail from diplay to send
 End Module
