7a7d35b1cc9f0be321c5a45e4323c737ed9bfb8b
Martin Klefas-Stennett <cleverbum@hotmail.com>
2019-01-24 12:00:54 +0000

    Todo list updates

--- DEP Excel Ribbon/todo.vb
+++ DEP Excel Ribbon/todo.vb
@@ -1,14 +1,14 @@
 ﻿Module todo
     '   This is a list of outstanding tasks, and contains no functional code. Everything should remain commented out - even if it loooks like code
 
-    ' create code to see if we've missed creating any tickets.
-    '       Take a long-view report (2 months or more)
-    '       Search for tickets that contain the first serial number (Reg or Ask only)
-    '       If no results then this line was missed.
+
+    ' Create button for end of day
+    '    checks If WC/TD registrations successful
+    '    closes tickets
 
 
-    '  check pivotmail with Duncan/Jenny
-    '  change pivotmail from diplay to send
+    ' Run BI Report for qty >10 orders.
+    ' new button for this only
 
 
 
